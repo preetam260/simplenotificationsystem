a071777bb9e72bcd6ccf4f93c66917b751026011
K Sasisekhara Preetam <kspreetam2608@gmail.com>
2026-05-17 01:02:28 +0530

    Update NotificationRepository.cs

--- Repositories/NotificationRepository.cs
+++ Repositories/NotificationRepository.cs
@@ -7,12 +7,12 @@ public class NotificationRepository : AbstractRepository<Notification>
         return items.FirstOrDefault(notification => notification.Id == id);
     }
 
-    public override void Update(Notification notification)
+    public override void Update(Notification notification) // contains updated notif details
     {
-        var notif = GetById(notification.Id);
+        var notif = GetById(notification.Id); //grabbing exisiting notification object
         if(notif != null)
         {
-            notif.UserId = notification.UserId;
+            notif.UserId = notification.UserId; // if you want to update who youre sending notif to
             notif.Type = notification.Type;
             notif.Message = notification.Message;
             notif.SentAt = DateTime.Now;
